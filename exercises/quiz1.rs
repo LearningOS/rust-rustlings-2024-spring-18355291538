96276475eecd7974750396523534490ac6765909
你要信我啊 <11962602+you-trust-me@user.noreply.gitee.com>
2024-05-02 20:37:22 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,20 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples (num:i32) -> i32 {
+    let price = if num == 35{
+        70
+    }else if num == 40{
+        80
+    }else if num == 41{
+        41
+    }else{
+        65
+    };
+    price
+}
 
 // Don't modify this function!
 #[test]
